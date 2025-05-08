9dd9ef18b5fc698307f4fcc69c89ec2896e7ac82
Ruth <ruthchavar@gmail.com>
2025-05-08 13:45:46 +0200

    third commit

--- index_gestor.php
+++ index_gestor.php
@@ -17,14 +17,15 @@ $recuento = $preparacion_recuento->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestor de Tareas</title>
+    <title>nimaList</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap">
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="shortcut icon" href="icon/list_task_to_do_list_icon_146882.svg" type="image/x-icon">
 </head>
 <body>
     <header>
-        <h1>Gestor de Tareas</h1>
+        <h1>minimaList</h1>
     </header>
     <div class="marcador-recuento">
     <h4>Recuento</h4>
